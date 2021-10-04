43237a8c30f7d3d7dffdadab52f7ea27ca22d193
benco2000 <benco2000@hotmail.fr>
2021-10-04 12:44:06 +0200

    Avancement TP2 Benjamin

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -1,17 +1,27 @@
 from lab2.utils import get_random_number_generator
+import numpy as np
+
+
+class MyClass(object):
+    def __init__(self, arg):
+        # attributes
+        self.arg = arg
+
+    def add_one_to_arg(self):
+        self.arg += 1
 
 
 class BoxWindow:
     """[summary]
     """
 
-    def __init__(self, args):
+    def __init__(self, bounds):
         """[summary]
 
         Args:
             args ([type]): [description]
         """
-        self.bounds = None
+        self.bounds = np.array(bounds)
 
     def __repr__(self):
         r"""BoxWindow: :math:`[a_1, b_1] \times [a_2, b_2] \times \cdots`
@@ -19,12 +29,16 @@ class BoxWindow:
         Returns:
             [type]: [description]
         """
-        return ""
+        return " "
 
     def __len__(self):
-        return
+        return len(self.bounds.shape[0])
+        return len(self.bounds)
 
-    def __contains__(self, args):
+    def __contains__(self, point):
+        assert len(point) == len(self)
+        for i in range (len(point)):
+
         return True or False
 
     def dimension(self):
